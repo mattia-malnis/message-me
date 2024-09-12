0f1c63bdb0a36ebb8528c0a01666b92c47c4b79b
Mattia Malnis <malnis.mattia@gmail.com>
2024-09-12 22:09:20 +0200

    Show a badge on the left sidebar when a user receive a new message

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -3,6 +3,7 @@ class ChatsController < ApplicationController
 
   prepend_before_action :authenticate_user!
   before_action :set_chat, only: [:show, :update]
+  before_action :set_recipient, only: [:show, :update]
 
   def index
     @chats = @profile.chats.includes(:profiles)
@@ -10,19 +11,32 @@ class ChatsController < ApplicationController
 
   def show
     @messages = @chat.messages.includes(:profile).ordered
-    @recipient = @chat.recipient(@profile.id)
   end
 
   def update
     @message = @chat.messages.new(message_params)
     @message.profile = @profile
     @message.save
+    broadcast_new_message
   end
 
   private
 
+  def broadcast_new_message
+    Turbo::StreamsChannel.broadcast_append_to(
+      "sidebar_profile_#{@recipient.nickname}",
+      target: "sidebar-chats",
+      partial: "chats/chat",
+        locals: { chat: @chat, recipient: @profile, new_msg: true }
+    )
+  end
+
   def set_chat
     @chat = @profile.chats.find_by!(token: params[:token])
+  end
+
+  def set_recipient
+    @recipient = @chat.recipient(@profile.id)
   end
 
   def message_params
---
+++ app/javascript/controllers/chat_display_controller.js
@@ -0,0 +1,32 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="chat-display"
+export default class extends Controller {
+  static values = { token: String };
+  static targets = ["messagesContainer"];
+
+  connect() {
+    // remove "new message" badge from the chat on the sidebar
+    this.hideBadge();
+
+    // scroll to the bottom of the chat
+    this.scrollToBottom();
+  }
+
+  // function to hide the new message badge
+  hideBadge() {
+    if (this.hasTokenValue) {
+      const badge = document.querySelector(`#chat_${this.tokenValue} .new-message-badge`);
+      if (badge) {
+        badge.style.display = "none";
+      }
+    }
+  }
+
+  // function to scroll the chat to the bottom
+  scrollToBottom() {
+    if (this.hasMessagesContainerTarget) {
+      this.messagesContainerTarget.scrollTop = this.messagesContainerTarget.scrollHeight;
+    }
+  }
+}
